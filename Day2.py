570818c18e1ab4150df8f8cc34b401eccaaae10f
Aryan4646 <aryansha2004@gmail.com>
2025-05-14 20:01:11 +0530

    Day 2 of 100 days of code

--- Day2.py
+++ Day2.py
@@ -6,4 +6,4 @@ num = int(input("Enter the number of people you want to divide the bill: "))
 bill = (total + (total*(tip/100)))/num
 
 print(f"The total bill was: {total}\nYou want to give tip of {tip}%\nNumber of person to divide the bill :{num}")
-# print(f"Each person has to pay: ${round(bill,2)}")
+print(f"Each person has to pay: ${round(bill,2)}")
