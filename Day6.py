c05c0b77767e18c0d382616bc86f10ef5a14b14e
Aryan4646 <aryansha2004@gmail.com>
2025-05-18 13:45:47 +0530

    Reeborg world Day 6

---
+++ Day6.py
@@ -0,0 +1,60 @@
+# Doing this challenge on https://reeborg.ca/reeborg.html
+
+# for hurdle 1
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def instru():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# for i in range(6):
+#     instru()
+
+# For hurdle 2
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def instru():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# while at_goal() != True:
+#     instru()
+
+# For hurdle 3
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def instru():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# while at_goal() != True:
+#     if front_is_clear():
+#         move()
+#     else:
+#         instru()
