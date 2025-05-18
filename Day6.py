ccd5b5fa32aefa9aa115ff7bfd7d96d2285510f4
Aryan4646 <aryansha2004@gmail.com>
2025-05-18 14:16:55 +0530

    Day 6 reeborg world

--- Day6.py
+++ Day6.py
@@ -58,3 +58,42 @@
 #         move()
 #     else:
 #         instru()
+
+# For hurdle 4
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def instru():
+#     turn_left()
+#     while right_is_clear() != True:
+#         move()
+#     else:
+#         turn_right()
+#         move()
+#         turn_right()
+#     while front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+# while at_goal() != True:
+#     if front_is_clear():
+#         move()
+#     else:
+#         instru()
+
+# Final Maze:
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# while at_goal() != True:
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
