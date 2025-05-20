e2d9591af032fdc3929bcfe5a7d3ec4ec8d0dc93
Aryan4646 <aryansha2004@gmail.com>
2025-05-20 10:42:23 +0530

    List of words in hangman

---
+++ list_hangman_Day_7.py
@@ -0,0 +1,10 @@
+hangman_words = [
+    "apple", "banana", "giraffe", "elephant", "zebra", "mountain", "python",
+    "oxygen", "jungle", "wizard", "planet", "rocket", "library", "puzzle",
+    "keyboard", "notebook", "umbrella", "whistle", "diamond", "kangaroo",
+    "volcano", "avalanche", "microscope", "chocolate", "squirrel", "treasure",
+    "nightmare", "backpack", "trampoline", "adventure", "lighthouse", "tsunami",
+    "cemetery", "dolphin", "spaceship", "galaxy", "reptile", "mirror", "battery",
+    "castle", "festival", "island", "pirate", "crystal", "invisible", "rhythm",
+    "shadow", "voyage", "machine"
+]
