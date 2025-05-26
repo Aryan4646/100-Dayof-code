ed26eebfb2425bdef3d5dcc58d09e73c8ea7e7c3
Aryan4646 <aryansha2004@gmail.com>
2025-05-26 10:29:00 +0530

    Sample data for day 14

---
+++ sample_data.py
@@ -0,0 +1,92 @@
+data = [
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 630,
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 510,
+        "description": "Footballer",
+        "country": "Argentina"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 430,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 400,
+        "description": "Reality TV personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 395,
+        "description": "Actor and professional wrestler",
+        "country": "United States"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 380,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 360,
+        "description": "Reality TV personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Beyoncé",
+        "follower_count": 320,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Nike",
+        "follower_count": 305,
+        "description": "Sports brand",
+        "country": "United States"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 290,
+        "description": "Musician",
+        "country": "Canada"
+    },
+    {
+        "name": "Khloé Kardashian",
+        "follower_count": 310,
+        "description": "Reality TV personality",
+        "country": "United States"
+    },
+    {
+        "name": "National Geographic",
+        "follower_count": 275,
+        "description": "Magazine",
+        "country": "United States"
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 290,
+        "description": "Model",
+        "country": "United States"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 290,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 270,
+        "description": "Cricketer",
+        "country": "India"
+    }
+]
