cfa587c6ca863faf8a6f4b25794253ab5aba6fba
Aryan4646 <aryansha2004@gmail.com>
2025-07-28 18:00:09 +0530

    Day 16 of 100 day of code

--- Day16.py
+++ Day16.py
@@ -1,1 +1,18 @@
 # Day 16 of 100 day of code
+# import turtle
+#
+# timmy = turtle.Turtle()
+# screen = turtle.Screen()
+# print(timmy)
+# print(screen.canvheight)
+# screen.exitonclick()
+
+from menu import Menu
+from coffe_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+student= ["C", "D", "A", "A", "B", "B", "A"]
+t = tuple(student)
+t.count("A")
+
+hhjhfsd
