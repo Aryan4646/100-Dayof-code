af8407dfb4998f34b176144a4eeb08693d014460
Aryan4646 <aryansha2004@gmail.com>
2025-08-01 15:43:20 +0530

    Day 22 Main logic of Pong game

--- Day22/main.py
+++ Day22/main.py
@@ -2,12 +2,15 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(height =600, width = 800)
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
+
+score = Scoreboard()
 
 c_ball = Ball()
 r_paddle = Paddle((375,0))
@@ -23,13 +26,23 @@ screen.onkey(l_paddle.Down,"s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(c_ball.movespeed)
     screen.update()
     c_ball.move()
-
 #     Detect collison with top and bottom walls
-    if c_ball.ycor() > 290 or c_ball.ycor() < -290 :
+    if c_ball.ycor() > 280 or c_ball.ycor() < -280 :
         c_ball.bounce()
+#   detect collision with r paddle
+    if c_ball.distance(r_paddle) <50 and c_ball.xcor() > 340 or c_ball.distance(l_paddle) < 50 and c_ball.xcor() < -340:
+        c_ball.paddle_bounce()
 
+    # R paddle collison
+    if c_ball.xcor() > 380:
+        c_ball.resetpositon()
+        score.lpoint()
+    # L paddle collision
+    if c_ball.xcor() < -380:
+        c_ball.resetpositon()
+        score.rpoint()
 
 screen.exitonclick()
