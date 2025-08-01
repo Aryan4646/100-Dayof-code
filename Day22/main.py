5565ad64d6c7078a0426c96c8c55378aa27cbaf7
Aryan4646 <aryansha2004@gmail.com>
2025-08-01 16:07:19 +0530

    Day 22 main file

--- Day22/main.py
+++ Day22/main.py
@@ -1,48 +1,48 @@
-from turtle import Turtle, Screen
+from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
-import time
 from scoreboard import Scoreboard
+import time
 
 screen = Screen()
-screen.setup(height =600, width = 800)
 screen.bgcolor("black")
-screen.title("Pong Game")
+screen.setup(width=800, height=600)
+screen.title("Pong")
 screen.tracer(0)
 
-score = Scoreboard()
-
-c_ball = Ball()
-r_paddle = Paddle((375,0))
-l_paddle = Paddle((-375,0))
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(r_paddle.Up,"Up")
-screen.onkey(r_paddle.Down,"Down")
-screen.onkey(l_paddle.Up,"w")
-screen.onkey(l_paddle.Down,"s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
-
 while game_is_on:
-    time.sleep(c_ball.movespeed)
+    time.sleep(ball.move_speed)
     screen.update()
-    c_ball.move()
-#     Detect collison with top and bottom walls
-    if c_ball.ycor() > 280 or c_ball.ycor() < -280 :
-        c_ball.bounce()
-#   detect collision with r paddle
-    if c_ball.distance(r_paddle) <50 and c_ball.xcor() > 340 or c_ball.distance(l_paddle) < 50 and c_ball.xcor() < -340:
-        c_ball.paddle_bounce()
+    ball.move()
 
-    # R paddle collison
-    if c_ball.xcor() > 380:
-        c_ball.resetpositon()
-        score.lpoint()
-    # L paddle collision
-    if c_ball.xcor() < -380:
-        c_ball.resetpositon()
-        score.rpoint()
+    #Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Detect L paddle misses:
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
