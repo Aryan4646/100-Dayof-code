7c7f4c7da6ffa9d648a0a24654e591b567fe4135
Aryan4646 <aryansha2004@gmail.com>
2025-08-01 16:11:10 +0530

    Restore Day22 directory after accidental GitHub deletion

--- Day22/ball.py
+++ Day22/ball.py
@@ -1,18 +1,30 @@
 from turtle import Turtle
 
+
 class Ball(Turtle):
+
     def __init__(self):
         super().__init__()
         self.color("white")
         self.shape("circle")
         self.penup()
-        self.xmove = 10
-        self.ymove = 10
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
 
     def move(self):
-        new_x = self.xcor() + self.xmove
-        new_y = self.ycor() + self.ymove
-        self.goto(new_x,new_y)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
 
-    def bounce(self):
-        self.ymove *= -1
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- Day22/paddle.py
+++ Day22/paddle.py
@@ -2,20 +2,22 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
+
     def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.goto(position[0], position[1])
+        self.goto(position)
 
-    def Up(self):
+    def go_up(self):
         new_y = self.ycor() + 20
-        self.goto(self.xcor(),y= new_y)
-    def Down(self):
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
         new_y = self.ycor() - 20
-        self.goto(self.xcor(),y= new_y)
+        self.goto(self.xcor(), new_y)
 
 
 
