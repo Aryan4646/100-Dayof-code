9437c5ddd2a8df3909c32f6596a9e8ea4daedd41
Aryan4646 <aryansha2004@gmail.com>
2025-08-03 11:26:39 +0530

    UPDATED scoreboard of snake game

--- Day21/scoreboard.py
+++ Day21/scoreboard.py
@@ -4,6 +4,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.penup()
         self.color("white")
         self.goto(0,265)
@@ -13,15 +14,22 @@ class Scoreboard(Turtle):
 
     def disp(self):
         self.clear()
-        t = self.write(f"Score: {self.score}",align = "center", font=("Arial", 24, "normal"))
+        t = self.write(f"Score: {self.score} High Score: {self.highscore}",align = "center", font=("Arial", 24, "normal"))
 
     def refres(self):
         self.disp()
         self.score += 1
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over", align = "center" , font =("Arial", 24, "normal") )
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game Over", align = "center" , font =("Arial", 24, "normal") )
+
+    def reset_score(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.disp()
+
 
 
 
